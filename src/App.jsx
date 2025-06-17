8a1cbd0c84339e694f567fd318021cb44546d2cc
satyabrat <satyabrat34@gmail.com>
2025-06-17 11:39:14 +0530

    readme file added

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,13 @@ import { items } from "./components/Data";
 import { useState } from "react";
 
 function App() {
-  // State to manage product data and cart
   const [data, setData] = useState(items);
   const [cart, setCart] = useState([]);
 
   return (
     <Router>
-      {/* Navbar with cart and search functionality */}
       <Navbar cart={cart} setData={setData} />
-
-      {/* Application routes */}
       <Routes>
-        {/* Home Page: Displays product list */}
         <Route
           path="/"
           element={<Product cart={cart} setCart={setCart} items={data} />}
